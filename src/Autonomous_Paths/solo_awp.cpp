4399ce3e490fc84c1555f19f0c2f956b3db042f8
Recut123 <tlittlen@gmail.com>
2026-01-01 18:00:54 -0700

    (1/1) Solo Win Point Tuning

--- src/Autonomous_Paths/solo_awp.cpp
+++ src/Autonomous_Paths/solo_awp.cpp
@@ -1,14 +1,17 @@
 #include "Autonomous/autonomous_definitions.h"
 #include "Autonomous_Functions/auton_functions.h"
 #include "RAT/path_follower.h"
-#include "Autonomous_Paths/solo_awp.h"
+#include "Robot/distance_calibration.h"
+#include "Robot/color_sorting.h"
 
+#include "Autonomous_Paths/solo_awp.h"
+#include <iostream>
 void SoloAWPAuton(void);
 
 Auton soloAWPAuton = {
     "Solo AWP",
     "Eat that trash teams",
-    63.25, 15.0, 270.0,
+    48.25, 14.5, 0.0,
     SoloAWPAuton
 };
 
@@ -18,46 +21,122 @@ void SoloAWPAuton(void) {
 
     matchloader.set(true);
     intake.spin(forward, 100, percent);
-    indexer.spin(forward, 100, percent);
-    task indexerTask = task(CheckMotorStallTask);
+
+    matchloader.set(true);
+    intake.spin(forward, 100, percent);
 
     Path matchload_path = PathGenerator::GeneratePath(
-    	{{48.00, 17.5},
-    	 {40.0, 33.5},
-    	 {48.00, 54},
-    	 {72.0, 41}
+    	{{48.0, 24.0},
+    	 {48.0, 46.0},
+    	 {64.5, 39.5},
     	},
     	50.0,
-    	25.0,
-    	6.0,
-    	0.8,
-    	2.5
+    	20.0,
+    	3.0,
+    	0.55,
+    	0.20
     );
 
-    FollowPath(matchload_path, forward, 16.0);
-    setDrivetrainSpeed(5);
+    FollowPath(matchload_path, forward, 12.0);
+    wait(450, msec);
 
-    wait(900, msec);
+    Path goal_path = PathGenerator::GeneratePath(
+    	{{56.0, 44.5},
+    	 {28.5, 44.0},
+    	},
+    	50.0,
+    	20.0,
+    	3,
+    	0.6,
+    	3.0
+    );
+    
+    // Reverse intake a bit to prevent jams
+   /*
+    goal_path.waypoints[3].onReach = []() {
+        std::cout << "Reversing!" << std::endl;
+        intake.spin(reverse, 100, percent);
+        wait(300, msec);
+        intake.stop();
+    };
+    */
+    FollowPath(goal_path, reverse, 18.0);
 
-    driveFor(-28, 100);
-
+    matchloader.set(false);
+    //setDrivetrainSpeed(-10);
+    //indexer_piston.set(true);
+    intake.spin(forward, 100, percent);
     hood.set(true);
-    indexer.spin(forward, 100, percent);
-    setDrivetrainSpeed(-100);
 
-    wait(200, msec);
+    // Color Sort!
 
-    driveFor(1, 100);
+    color_sort_mode allianceColor = colorSortMode;
+    color otherColor;
 
-    wait(600, msec);
+    if (allianceColor == RED) {
+        otherColor = color::blue;
+    } else {
+        otherColor = color::red;
+    }
 
+    // Wait until we see blue, or theres a timeout
+   int startScoreTime = Brain.Timer.system();
+    bool scoring = true;
+
+    color_sensor.objectDetectThreshold(1);
+
+    while (scoring) {
+        if (color_sensor.color() == red) {
+            std::cout << "Seeing: Red"  << std::endl;
+        } else if (color_sensor.color() == blue) {
+            std::cout << "Seeing: Blue"  << std::endl;
+        } else if (color_sensor.color() == cyan) {
+            std::cout << "Seeing: Cyan"  << std::endl;
+        } else {
+            std::cout << "Seeing: None"  << std::endl;
+        }
+
+        if (color_sensor.isNearObject())  {
+            if ((Brain.Timer.system() - startScoreTime) > 1750) {
+                scoring = false;
+            }
+
+            if (otherColor == blue) {
+                if (color_sensor.color() == blue || color_sensor.color() == cyan) {
+                    scoring = false;
+                }
+            } else if (otherColor == red) {
+                if (color_sensor.color() == red) {
+                    scoring = false;
+                }
+            }
+        } else {
+            scoring = false;
+        }
+        wait(20, msec);
+    }
+
+    std::cout << "sorted" << std::endl;
+
+    // Stop scoring
+/*  indexer_piston.set(false);
+    intake.spin(forward, 100, percent);
+    driveFor(6, 100);
+
+    // Bump
+    hood.set(false);
+    setDrivetrainSpeed(-50);
+    wait(500, msec);
+*/
+    hood.set(false);
+    indexer_piston.set(true);
     Path middle_goal_ball_path = PathGenerator::GeneratePath(
-    	{{37.76, 48.59},
-    	 {48.29, 47.71},
-    	 {48.88, 33.95},
-    	 {31.34, 32.20},
-    	 {26.00, 24.00},
-    	 {27.00, -26.00}
+    	{{35.76, 48.59},
+    	 {46.29, 47.71},
+    	 {44.88, 33.95},
+    	 {27.34, 32.20},
+    	 {20.50, 24.00},
+    	 {24.00, -30.00}
     	},
     	60.0,
     	70.0,
@@ -65,29 +144,35 @@ void SoloAWPAuton(void) {
     	0.6019,
     	3.0
     );
+    
+    middle_goal_ball_path.waypoints[3].onReach = []() {
+        indexer_piston.set(false);
+    };
+    middle_goal_ball_path.waypoints[4].onReach = []() {
+        indexer_piston.set(false);
+    };
+    middle_goal_ball_path.waypoints[5].onReach = []() {
+        indexer_piston.set(false);
+    };
+
 
     FollowPath(middle_goal_ball_path, forward, 16.0);
 
     //driveTo(-13, -13, 100, reverse);
 
-    wait(300, msec);
-    pointAt(0, 0, 100, reverse);
-    driveFor(-19, 100);
-    indexer.spin(forward, 55, percent);
-    hood.set(true);
-    driveFor(2, 30);
+    //wait(300, msec);
+    pointAt(3.0, -3.5, 100, reverse);
 
-    wait(1000, msec);
+    driveFor(-17.5, 100);
+    indexer_piston.set(true);
+    wait(600, msec);
 
-    indexer.stop();
-    hood.set(false);
     matchloader.set(true);
-
+    indexer_piston.set(false);
     // Get other matchloader
 
     Path matchload_path_1 = PathGenerator::GeneratePath(
-    	{{48.00, -17.5},
-    	 {40.0, -33.5},
+    	{{35.0, -33.5},
     	 {48.00, -55.5},
     	 {72.0, -45}
     	},
@@ -99,22 +184,61 @@ void SoloAWPAuton(void) {
     );
 
     FollowPath(matchload_path_1, forward, 16.0);
-    setDrivetrainSpeed(5);
+    //setDrivetrainSpeed(5);
 
-    wait(900, msec);
+    wait(450, msec);
 
-    driveFor(-28, 100);
+    Path goal_path1 = PathGenerator::GeneratePath(
+    	{{56.0, -44.5},
+    	 {32.5, -51.0},
+    	},
+    	50.0,
+    	10.0,
+    	3,
+    	0.6,
+    	3.0
+    );
+    
+    FollowPath(goal_path1, reverse, 18.0);
 
-    left_drive.spin(reverse, 100, percent);
-    right_drive.spin(reverse, 100, percent);
-
+    matchloader.set(false);
+    //setDrivetrainSpeed(-10);
+    intake.spin(forward, 100, percent);
     hood.set(true);
-    indexer.spin(forward, 100, percent);
-    setDrivetrainSpeed(-100);
 
-    wait(100, msec);
+    /* Color Sort!
 
-    driveFor(1, 100);
+    color_sort_mode allianceColor = colorSortMode;
+    color otherColor;
 
-    wait(600, msec);
+    if (allianceColor == RED) {
+        otherColor = color::blue;
+    } else {
+        otherColor = color::red;
+    }
+
+    // Wait until we see blue, or theres a timeout
+    int startScoreTime = Brain.Timer.system();
+    bool scoring = true;
+
+
+    //while (scoring) {
+    //    if ((Brain.Timer.system() - startScoreTime) > 1750) {
+    //        scoring = false;
+    //    }
+    //    if (otherColor == blue) {
+    //        if (color_sensor.color() == blue) {
+    //            scoring = false;
+    //        }
+    //    } else if (otherColor == red) {
+    //        if (color_sensor.color() == red) {
+    //            scoring = false;
+    //        }
+    //    }
+    //    wait(20, msec);
+    //}
+    */
+    
+    wait(1250, msec);
+
 }
